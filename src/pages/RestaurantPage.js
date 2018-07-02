12d369117703edc097b4d8309603dcd1ac006595
Jenny <jennyngo.dev@gmail.com>
2018-07-02 15:06:31 -0400

    created restaurantpage component

---
+++ src/pages/RestaurantPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class RestaurantPage extends Component {
+  render() {
+    return (
+      <div>
+        <p>you're on the restaurant page</p>
+      </div>
+    )
+  }
+}
